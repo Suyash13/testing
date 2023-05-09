bc78e6420211cf1984fc8ddf215141029ec19392
Suyash13 <76859052+Suyash13@users.noreply.github.com>
2023-05-09 10:23:26 +0530

    Update SRTF.cpp

--- SRTF.cpp
+++ SRTF.cpp
@@ -1,88 +1,54 @@
-// S.R.T.F. Scheduling Policy
-// By Suyash Pandey: 2020KUEC2045
-#include <bits/stdc++.h>
+#include<iostream>
+
 using namespace std;
-
-struct process
-{
-    int pid;
-    int arrival_time;
-    int burst_time;
-};
-
-// Comparison function to sort the processes based on burst time
-bool cmp(process a, process b)
-{
-    return a.burst_time < b.burst_time;
-}
-
-void SRTF(vector<process> processes)
-{
-    int n = processes.size();
-    int current_time = 0, completed = 0, prev = 0;
-    float total_wait_time = 0, total_turnaround_time = 0;
-
-    // Priority queue to store the processes based on remaining burst time
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    // Sort the processes based on arrival time
-    sort(processes.begin(), processes.end(), [](process a, process b)
-         { return a.arrival_time < b.arrival_time; });
-
-    cout << "PID\tA.T.\tB.T.\tC.T." << endl;
-    while (completed != n)
-    {
-        for (int i = prev; i < n; i++)
-        {
-            if (processes[i].arrival_time <= current_time)
-            {
-                pq.push({processes[i].burst_time, processes[i].pid});
-                prev++;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        if (pq.empty())
-        {
-            current_time++;
-            continue;
-        }
-
-        pair<int, int> curr = pq.top();
-        pq.pop();
-
-        int pid = curr.second;
-        int burst_time = curr.first;
-
-        total_wait_time += current_time - processes[pid].arrival_time;
-        total_turnaround_time += current_time + burst_time - processes[pid].arrival_time;
-
-        current_time += burst_time;
-        completed++;
-
-        cout << processes[pid].pid + 1 << "\t" << processes[pid].arrival_time << "\t" << processes[pid].burst_time << "\t" << current_time << endl;
-    }
-
-    float avg_wait_time = total_wait_time / n;
-    float avg_turnaround_time = total_turnaround_time / n;
-    cout << "Average Waiting Time: " << avg_wait_time << endl;
-    cout << "Average Turnaround Time: " << avg_turnaround_time << endl;
-}
-
 int main()
 {
-    // Input of Process IDs, Arrival Times and Burst Times
-    vector<process> processes = {
-        {0, 0, 5},
-        {1, 1, 3},
-        {2, 2, 8},
-        {3, 3, 6},
-        {4, 4, 1}};
+    int AT[10],BT[10],x[10],WT[10],TAT[10],CT[10],i,j,smallest,count=0,time,n;
+    double avg_WT=0,avg_TAT=0,end;
 
-    SRTF(processes);
+    cout<<"\nEnter the number of Processes: ";
+    cin>>n;
+    for (i=0;i<n;i++)
+    {
+        cout<<"enter Arival time P["<<i<<"]:";
+        cin>>AT[i];
+    }
+    cout<<endl;
+    for (i=0;i<n;i++)
+    {
+        cout<<"enter Brust time P["<<i<<"]:";
+        cin>>BT[i];
+    }
+    for (i=0;i<n;i++)
+        x[i]=BT[i];
 
-    return 0;
+    BT[9]=9999;
+    for(time=0; count!=n; time++)
+    {
+        smallest=9;
+        for (i=0;i<n;i++)
+        {
+            if(AT[i]<=time && BT[i]<BT[smallest] && BT[i]>0 )
+                smallest=i;
+        }
+        BT[smallest]--;
+
+        if(BT[smallest]==0)
+        {
+            count++;
+            end=time+1;
+            CT[smallest] = end;
+            WT[smallest] = end - AT[smallest] - x[smallest];
+            TAT[smallest] = end - AT[smallest];
+        }
+    }
+    cout <<" Processes "<<"  "<< "Burst time "<<"  "<< "Arrival time "<< "\t"<<" Waiting time " <<"\t"<< " Turn around time"<<"  "<<" completion-time "<<"\n";
+
+    for (i=0;i<n;i++)
+    {
+        cout << "   " << i+1 << "\t\t" << x[i] << "\t\t" << AT[i] << "\t\t" << WT[i] << "\t\t" << TAT[i] << "\t\t" << CT[i] <<endl;
+        avg_WT = avg_WT + WT[i];
+        avg_TAT = avg_TAT + TAT[i];
+    }
+    cout<<"\nAverage waiting time ="<<avg_WT<<endl<<"Average Turnaround time ="<<avg_TAT<<endl;
 }
